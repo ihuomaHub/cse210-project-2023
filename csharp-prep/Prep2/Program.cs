b22d8b103928ff2706fc047a27c51aec652ba607
ihuomajohn <ano22002@byui.edu>
2023-04-18 17:18:36 +0100

    Prep 2 part 1 completed

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string userInput = Console.ReadLine();
+        int X = int.Parse(userInput);
+
+        if (X >= 90)
+        {
+            Console.WriteLine("Your grade score is A!");
+        }
+        else if (X >= 80 && X < 90)
+        {
+            Console.WriteLine("Your grade score is B!");
+        }
+        else if (X >= 70 && X < 80)
+        {
+            Console.WriteLine("Your grade score is C!");
+        }
+        else if (X >= 60 && X < 70)
+        {
+            Console.WriteLine("Your grade score is D!");
+        }
+        else
+        {
+            Console.WriteLine("Your grade score is F!");
+        }
     }
 }
