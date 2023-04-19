93e7264dc16ca15e4ad082e1ac601fe083f4849d
ihuomajohn <ano22002@byui.edu>
2023-04-19 04:32:06 +0100

    Prep 3 part1 completed

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("What is the magic number? ");
+        string input = Console.ReadLine();
+        Console.Write("Guess the number: ");
+        string guess = Console.ReadLine();
+        int number = int.Parse(input);
+        int userGuess = int.Parse(guess);
+
+        if (userGuess > number)
+        {
+            Console.WriteLine("Lower");
+        }
+        else if (userGuess < number)
+        {
+            Console.WriteLine("higher");
+        }
+        else
+        {
+            Console.WriteLine("You guess it!");
+        }
     }
 }
