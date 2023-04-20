9de8b8d8f2575cbc782af66cd79ea1fc58d8c881
ihuomajohn <ano22002@byui.edu>
2023-04-20 07:31:58 +0100

    Prep3 Core Requirement completed

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,15 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        int magicNumber = 0;
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
 
         int userGuess = -1;
         
         do
         {
-            Console.Write("What is the magic number? ");
-            magicNumber = int.Parse(Console.ReadLine());
-           
             Console.Write("Guess the number: ");
             userGuess = int.Parse(Console.ReadLine());
 
