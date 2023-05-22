90a9a45f10c845621889905a94a50834c90f9d56
ihuomajohn <ano22002@byui.edu>
2023-05-22 19:35:36 +0100

    Resume completed

--- prepare/Learning02/Job.cs
+++ prepare/Learning02/Job.cs
@@ -4,7 +4,6 @@ public class Job
     public string _jobTitle;
     public int _startYear;
     public int _endYear;
-    
     public void Display()
     {
         Console.WriteLine($"{_jobTitle} ({_companyName}) {_startYear}-{_endYear}");
