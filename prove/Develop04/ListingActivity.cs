1861a43662992b777cc0020e20264db2362740e7
ihuomajohn <ano22002@byui.edu>
2023-06-14 05:45:34 +0100

    Mindfullness activity still developing

--- prove/Develop04/ListingActivity.cs
+++ prove/Develop04/ListingActivity.cs
@@ -1,32 +1,61 @@
-public class ListingActivity : Activity
+using System;
+using System.Collections.Generic;
+
+public class ListingActivity : Mindfullness
 {
     private string areaOfReflection;
     private List<string> promptOptions;
     private int itemCounter;
-}
 
-public void SetAreaOfReflection(string area)
-{
-    areaOfReflection = area;
-}
-public void AddPromptOption(string prompt)
-{
-    promptOptions.Add(prompt);
-}
-public void RemovePromptOption(string prompt)
-{
-    promptOptions.Remove(prompt);
-}
-public string GetRandomPrompt()
-{
-    return promptOptions[new Random().Next(promptOptions.Count)];
-}
+    public ListingActivity()
+    {
+        ActivityName = "Listing Activity";
+        Description = "This activity will help you reflect on the good things in your life by having you list as many things as you can in a certain area.";
+    }
 
-public void DisplayPrompt()
-{
-    Console.WriteLine("Listing Activity Prompt:");
-}
-public void RunActivity()
-{
-    StartActivity();
+    public void SetAreaOfReflection(string area)
+    {
+        areaOfReflection = area;
+    }
+
+    public void AddPromptOption(string prompt)
+    {
+        promptOptions.Add(prompt);
+    }
+
+    public void RemovePromptOption(string prompt)
+    {
+        promptOptions.Remove(prompt);
+    }
+
+    public string GetRandomPrompt()
+    {
+        Random random = new Random();
+        return promptOptions[random.Next(promptOptions.Count)];
+    }
+
+    public override void RunActivity()
+    {
+        base.RunActivity();
+
+        DisplayPrompt();
+        PauseWithSpinner(2);
+
+        itemCounter = 0;
+        while (itemCounter < Duration)
+        {
+            Console.WriteLine("Enter an item for reflection:");
+            string item = Console.ReadLine();
+            itemCounter++;
+        }
+
+        Console.WriteLine("Number of items entered: {0}", itemCounter);
+    }
+
+    private void DisplayPrompt()
+    {
+        Console.WriteLine("Listing Activity Prompt:");
+        string randomPrompt = GetRandomPrompt();
+        Console.WriteLine(randomPrompt);
+    }
 }
