fdbdeb0d330606730aa94f0ec44a9e8c13279a23
ihuomajohn <ano22002@byui.edu>
2023-06-14 04:56:26 +0100

    Mindfullness activity still developing

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -4,7 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World 04!");
+        Console.WriteLine("WELCOME TO THE MINDFULNNESS ACTIVITY");
+        Console.WriteLine("THANKS FOR TAKING UP THIS TASK! HAVE FUN!");
         
     }
 }
